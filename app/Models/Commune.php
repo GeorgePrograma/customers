4bd14622be0caa42d663dc566568af17e6cdf46c
GeorgePrograma <jorge.beristain.hernandez@gmail.co>
2022-03-18 17:42:23 -0600

    Proyecto con Modelo, Migraciones y base de datos inicial

---
+++ app/Models/Commune.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Commune extends Model
+{
+    use HasFactory;
+
+    //Relacion de Muchos a Uno
+    public function Regions(){
+        return $this->belongsTo("App\Models\Region");
+    }
+
+    //Relacion de Uno a Muchos
+    public function Customers(){
+        return $this->hasMany("App\Models\Customers");
+    }
+
+}
